3af2b545c1b38b46dd5bd37d3f3809096ce13f9f
Christopher Johnstone <chrisj97@proton.me>
2026-09-02 17:17:36 +0100

    feat: added miniflux deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,55 +14,56 @@ module "envoy" {
   namespace  = local.namespace
 }
 
-# TODO: Set up database
 # TODO: Set up service
-# resource "kubernetes_deployment_v1" "miniflux" {
-#   depends_on = [kubernetes_namespace_v1.namespace]
-#   metadata {
-#     name = "minifix"
-#     labels = {
-#       app = "miniflux"
-#     }
-#   }
-#   spec {
-#     replicas = 2
-#     selector {
-#       match_labels = {
-#         app = "miniflux"
-#       }
-#     }
-#     template {
-#       metadata {
-#         labels = {
-#           app = "miniflux"
-#         }
-#       }
-#       spec {
-#         container {
-#           name  = "miniflux"
-#           image = "docker.io/miniflux/miniflux:latest"
-#           env {
-#             name  = "DATABASE_URL"
-#             value = "postgres://miniflux:*password*@*dbhost*/miniflux?sslmode=disable"
-#           }
-#           env {
-#             name  = "RUN_MIGRATIONS"
-#             value = "1"
-#           }
-#           env {
-#             name  = "CREATE_ADMIN"
-#             value = "1"
-#           }
-#           env {
-#             name  = "ADMIN_USERNAME"
-#             value = "*username*"
-#           }
-#           env {
-#             name  = "ADMIN_PASSWORD"
-#             value = "*password*"
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
+resource "kubernetes_deployment_v1" "miniflux" {
+  depends_on = [kubernetes_namespace_v1.namespace]
+  metadata {
+    namespace = local.namespace
+    name      = "miniflux"
+    labels = {
+      app = "miniflux"
+    }
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "miniflux"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "miniflux"
+        }
+      }
+      spec {
+        container {
+          name  = "miniflux"
+          image = "docker.io/miniflux/miniflux:latest"
+          env {
+            name  = "DATABASE_URL"
+            value = "postgres://miniflux:${var.miniflux_db_password}@${var.miniflux_db_host}/miniflux?sslmode=${var.miniflux_db_ssl}"
+          }
+          env {
+            name  = "RUN_MIGRATIONS"
+            value = "1"
+          }
+          env {
+            name  = "CREATE_ADMIN"
+            value = "1"
+          }
+          env {
+            name  = "ADMIN_USERNAME"
+            value = var.miniflux_admin_username
+          }
+          # TODO: Password exposed
+          env {
+            name  = "ADMIN_PASSWORD"
+            value = var.miniflux_admin_password
+          }
+        }
+      }
+    }
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,3 +18,34 @@ variable "envoy_gateway_version" {
   nullable    = false
   default     = "1.8.1"
 }
+
+variable "miniflux_db_host" {
+  description = "Host address for miniflux database"
+  type        = string
+  nullable    = false
+}
+
+variable "miniflux_db_password" {
+  description = "Password for miniflux database user"
+  type        = string
+  nullable    = false
+}
+
+variable "miniflux_db_ssl" {
+  description = "Wether to use to use SSL for miniflux database connection"
+  type        = string
+  nullable    = false
+  default     = "disable"
+}
+
+variable "miniflux_admin_username" {
+  description = "Username to give miniflux admin user"
+  type        = string
+  nullable    = false
+}
+
+variable "miniflux_admin_password" {
+  description = "Password to give miniflux admin user"
+  type        = string
+  nullable    = false
+}
